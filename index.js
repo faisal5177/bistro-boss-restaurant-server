109e62d365cfbfcd2fba876b5565d4dee716507e
faisal <mdfaisal51777@gmail.com>
2025-10-12 17:50:42 +0600

    added an cart

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
 
     app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      console.log('Trying to delete cart item with id:', id); 
+      console.log('Trying to delete cart item with id:', id);
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
